d07736f507b0dad0ea5c2da78c4dd6ad26258623
Mike <hello.mikle@mail.ru>
2018-06-18 09:21:22 +0300

    first

---
+++ Parcer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parcer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
